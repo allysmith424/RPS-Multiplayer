fa6ff19099a9b75d82d7f64b4d30767c0b58e2c0
Ally Smith <allysmith@Allys-MacBook-Air.local>
2018-02-01 16:53:16 -0600

    initial files for rock paper scissors game

---
+++ assets/javascript/logic.js
@@ -0,0 +1,65 @@
+// variables
+
+	// player 1 yourChoice
+
+	// player 1 choice
+
+	// player 2 yourChoice
+
+	// player 2 choice
+
+
+	// player 1 wins
+
+	// player 1 losses
+
+	// ties
+
+	// player 2 wins
+
+	// player 2 losses
+
+
+	// player 1 comments
+
+	// player 2 comments
+
+	// choiceCounter
+
+
+// if / else logic for comparison
+
+// firebase
+
+	// only allows two players
+
+		// ask for name
+
+		// notify each player if they're player 1 and waiting for player 2 OR player 2, let's begin
+
+	// when you choose
+
+		// update FB with yourChoice
+
+		// update player's own DOM with choice
+
+		// if choiceCounter = 1
+
+			// notify opponent that they've chosen
+
+	// other player chooses
+
+		// update FB with yourChoice
+
+		// update player's DOM with their choice
+
+	// display results
+
+		// reveal both choices on screen
+
+		// update counter variables
+
+		// reset yourChoice
+
+		// 
+
